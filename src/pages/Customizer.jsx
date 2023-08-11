bc30d2d6d4324a8629d881a7a283cfce7ed7d822
Numan Ijaz <naumanijaz928@gmail.com>
2023-08-11 21:13:49 +0500

    first project tabs added

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -29,7 +29,19 @@ const Customizer = () => {
   });
 
   // show tab content depending on the activeTab
-  const generateTabContent = () => {};
+  const generateTabContent = () => {
+    switch (activeEditorTab) {
+      case "colorpicker":
+        return <ColorPicker />;
+
+      case "filepicker":
+        return <FilePicker />;
+      case "aipicker":
+        return <AIPicker />;
+      default:
+        return null;
+    }
+  };
   return (
     <AnimatePresence>
       {!snap.intro && (
@@ -42,8 +54,15 @@ const Customizer = () => {
             <div className="flex items-center min-h-screen">
               <div className="editortabs-container tabs">
                 {EditorTabs.map((tab) => (
-                  <Tab key={tab.name} tab={tab} handleClick={() => {}} />
+                  <Tab
+                    key={tab.name}
+                    tab={tab}
+                    handleClick={() => {
+                      setActiveEditorTab(tab.name);
+                    }}
+                  />
                 ))}
+                {generateTabContent()}
               </div>
             </div>
           </motion.div>
